ba27613182f4925483e19f463f5ec0630760c818
Hiroshi MORIMOTO <kikoro.jp@gmail.com>
2023-07-12 12:08:09 +0900

    blockquoteのレイアウト調整

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,5 +2,8 @@
  * ロード時の初期化処理
  */
 window.onload = () => {
-    
+    //blockquote内テキストの先頭改行と各行頭スペースを除去
+    document.querySelectorAll('blockquote').forEach(ele => {
+        ele.innerText = ele.innerText.replace(/^\n/, '').split('\n').map(line => line.replace(/^\s+/, '')).join('\n');
+    });
 }
